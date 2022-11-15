49ddc2c79dcf3c96ec2651d5c776a5c79940353a
RomainSouteirat <romainsout@gmail.com>
2022-11-15 17:46:08 +0100

    création de la classe Commercant

--- PierreSabre/src/histoire/HistoireTP4.java
+++ PierreSabre/src/histoire/HistoireTP4.java
@@ -1,17 +1,24 @@
 package histoire;
 
+import personnages.Commercant;
 import personnages.Humain;
 
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
 		Humain Prof = new Humain("Prof","kombucha",54 );
+		Commercant Marco = new Commercant("Marco", 10);
 		
-		Prof.direBonjour();
-		Prof.acheter("une boisson", 12);
-		Prof.Boire();
-		Prof.acheter("un jeu", 2);
-		Prof.acheter("un kimono", 50);
+//		Prof.direBonjour();
+//		Prof.acheter("une boisson", 12);
+//		Prof.Boire();
+//		Prof.acheter("un jeu", 2);
+//		Prof.acheter("un kimono", 50);
+		
+		Marco.direBonjour();
+		Marco.seFaireExtorquer();
+		Marco.recevoir(15);
+		Marco.Boire();
 	}
 
 }
---
+++ PierreSabre/src/personnages/Commercant.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Commercant extends Humain {
+	
+	public Commercant(String nom, int argent) {
+		super(nom, "thé", argent);
+	}
+	
+	public int seFaireExtorquer() {
+		int argentVole = getArgent();
+		perdreArgent(argentVole);
+		parler("J’ai tout perdu! Le monde est trop injuste...");
+		return argentVole;
+	}
+	
+	public void recevoir(int argentdonne) {
+		parler(argentdonne + " sous ! je te remercie généreux donateur!");
+		gagnerArgent(argentdonne);
+	}
+}
