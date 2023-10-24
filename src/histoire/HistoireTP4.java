81c852d36855696c9bf274b0946c481e78cd258c
RomainSouteirat <romainsout@gmail.com>
2023-10-24 16:51:19 +0200

    creation de la classe Humain

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,5 +1,15 @@
 package histoire;
 
-public class HistoireTP4 {
+import personnages.Humain;
 
+public class HistoireTP4 {
+	
+	public static void main(String[] args) {
+		Humain romain = new Humain(54, "romain", "Coca");
+		romain.direBonjour();
+		romain.boire();
+		romain.acheter("boisson", 12);
+		romain.acheter("jeu", 2);
+		romain.acheter("kimono", 50);
+	}
 }
---
+++ src/personnages/Humain.java
@@ -0,0 +1,59 @@
+package personnages;
+
+public class Humain {
+	private int argent;
+	private String nom;
+	private String boissonFav;
+	
+	public Humain(int argent, String nom, String boissonFav) {
+		this.argent = argent;
+		this.nom = nom;
+		this.boissonFav = boissonFav;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public String getBoissonFav() {
+		return boissonFav;
+	}
+	
+	private String prendreParole() {
+		String texte = "(" + nom + ") - ";
+		return texte;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + texte);
+		}
+	
+	public void direBonjour() {
+		parler("Bonjour je m'appelle " + nom + " et j'aime boire du " + boissonFav + ".");
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFav + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix > argent) {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+		} else {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");	
+			argent -= prix;
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+}
